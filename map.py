c480fdf6f8107a6003ccb1d9aa809ae60e08f5c8
QueenieeQ <xuanquyhnvn@gmail.com>
2024-05-15 16:04:14 +0700

    change map style

--- map.py
+++ map.py
@@ -8,7 +8,7 @@ geolocator = Nominatim(user_agent="my_geocoder")
 location = geolocator.geocode("Hanoi, Vietnam")
 
 # Create a Folium map centered around the location
-m = folium.Map(location=[location.latitude, location.longitude], zoom_start=6)
+m = folium.Map(location=[location.latitude, location.longitude], zoom_start=6,tiles="cartodb positron")
 
 # Load GeoJSON data for Vietnam's provinces
 with open('diaphantinh.geojson') as f:
@@ -19,6 +19,8 @@ folium.GeoJson(province_data, name='province borders').add_to(m)
 
 # Add a marker for the location
 folium.Marker([location.latitude, location.longitude], popup="Hanoi, Vietnam").add_to(m)
+
+folium.TileLayer('cartodb positron', opacity=0.5).add_to(m)
 
 # Save the map to an HTML file and open it in a web browser
 m.save('vietnam_map_with_provinces.html')
