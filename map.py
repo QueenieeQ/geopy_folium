b6596c7a0b1375c24fa38f1fe29994bc439f9bbf
QueenieeQ <xuanquyhnvn@gmail.com>
2024-05-15 15:26:24 +0700

    display map province layer on folium using geojson data

--- map.py
+++ map.py
@@ -8,10 +8,17 @@ geolocator = Nominatim(user_agent="my_geocoder")
 location = geolocator.geocode("Hanoi, Vietnam")
 
 # Create a Folium map centered around the location
-m = folium.Map(location=[location.latitude, location.longitude], zoom_start=10)
+m = folium.Map(location=[location.latitude, location.longitude], zoom_start=6)
+
+# Load GeoJSON data for Vietnam's provinces
+with open('diaphantinh.geojson') as f:
+    province_data = f.read()
+
+# Add GeoJSON layer for province borders
+folium.GeoJson(province_data, name='province borders').add_to(m)
 
 # Add a marker for the location
 folium.Marker([location.latitude, location.longitude], popup="Hanoi, Vietnam").add_to(m)
 
 # Save the map to an HTML file and open it in a web browser
-m.save('hanoi_map.html')
+m.save('vietnam_map_with_provinces.html')
